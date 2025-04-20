b66811eff98a303fd4521372d6c4ce3fc1fe22b9
satabios <25417284+satabios@users.noreply.github.com>
2025-04-20 13:02:03 -0700

    updated vsce-build-local

--- scripts/vsce-build-local.sh
+++ scripts/vsce-build-local.sh
@@ -4,9 +4,10 @@ rm -rf node_modules
 rm -rf .vscode-test
 rm -rf *.vsix
 
-#  npm install diff@5.1.0 onnxruntime-node@1.17.0 protobufjs@7.5.0
-
 vsce package
 code --install-extension *.vsix
+
+
+# Publish to marketplace
 # vscde publish
 
--- scripts/vsce-push.sh
+++
@@ -1,5 +0,0 @@
-# Get version from package.json
-VERSION=$(jq -r '.version' package.json)
-
-git tag -a "v$VERSION" -m "Release version $VERSION"
-git push origin "v$VERSION"
